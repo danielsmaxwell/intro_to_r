bdd53ec35643b8d8ac34e9dc585ec304cc32d525
Daniel Maxwell <dsm_direct@yahoo.com>
2017-06-22 13:51:50 -0400

    Fixed all opar(par) issues.

--- module_3/activities_chp_3.R
+++ module_3/activities_chp_3.R
@@ -1,37 +1,4 @@
-
-# Create dataframe from data file.
-energy <- read.csv("energy_expend.csv", stringsAsFactors = FALSE)
-
-# Two ways to create separate vectors for each stature factor.
-lst <- split(energy$expend, energy$stature)
-
-lean  <- energy$expend[energy$stature == "lean"]
-obese <- energy$expend[energy$stature == "obese"]
-
-# Set graphic device parameters to 2 rows and 1 column.
-par(mfrow = c(2, 1))    
- 
-# Graph the two vectors.
-hist(lean, breaks = 10, xlim = c(5, 13), ylim = c(0, 4), col = "lightblue")
-hist(obese, breaks = 10, xlim = c(5, 13), ylim = c(0, 4), col = "lemonchiffon")
-
-# Restore graphic device to 1 row, 1 column.
-par(mfrow = c(1, 1))    
-
-# Two ways to create the boxplot.
-boxplot(lean, obese, names = c("Lean","Obese"))
-boxplot(energy$expend ~ energy$stature, names = c("Lean","Obese"))
-
-# Create four stripcharts.
-opar <- par(no.readonly = TRUE)
-par(mfrow = c(2,2), mex = 0.8, mar = c(3, 3, 2, 1) + .1)
-
-stripchart(energy$expend ~ energy$stature)
-stripchart(energy$expend ~ energy$stature, method = "stack")
-stripchart(energy$expend ~ energy$stature, method = "jitter")
-stripchart(energy$expend ~ energy$stature, method = "jitter", jitter = .03)
-
-par() <- opar
+# Basic Plot Code -------------------------------------------------------------
 
 # Create dataframe from the data file
 poverty <- read.delim("family_poverty.txt", sep = "\t", stringsAsFactors = FALSE)
@@ -41,17 +8,19 @@ ten  <- subset(poverty, year > 2005)
 
 # Plot poverty % by year.
 plot(ten$year, ten$family_poverty_pct,
-               xlab = "Year",
-               ylab = "Families in Poverty %",
-               ylim = c(9, 13), 
-               type = "b", 
-               cex  = .75,
-               lty  = 3,
-               col  = "blue",
-               pch  = 1)
+     xlab = "Year",
+     ylab = "Families in Poverty %",
+     ylim = c(9, 13), 
+     type = "b", 
+     cex  = .75,
+     lty  = 3,
+     col  = "blue",
+     pch  = 1)
 
 # Reset the parameters.
-par() <- opar
+par(opar)
+
+# Multi-Line Plot Code -------------------------------------------------------
 
 tmp <- read.delim("race_poverty.txt", sep = "\t", stringsAsFactors = FALSE)
 
@@ -93,5 +62,43 @@ legend("bottomright",
        lty   = c(3, 2), 
        pch   = c(1, 0),
        col   = c("blue","red"))
+
+# Histogram, Boxplot, Stripchart Code ----------------------------------------
+
+# Create dataframe from data file.
+energy <- read.csv("energy_expend.csv", stringsAsFactors = FALSE)
+
+# Two ways to create separate vectors for each stature factor.
+lst <- split(energy$expend, energy$stature)
+
+lean  <- energy$expend[energy$stature == "lean"]
+obese <- energy$expend[energy$stature == "obese"]
+
+# Set graphic device parameters to 2 rows and 1 column.
+par(mfrow = c(2, 1))    
+ 
+# Graph the two vectors.
+hist(lean, breaks = 10, xlim = c(5, 13), ylim = c(0, 4), col = "lightblue")
+hist(obese, breaks = 10, xlim = c(5, 13), ylim = c(0, 4), col = "lemonchiffon")
+
+# Restore graphic device to 1 row, 1 column.
+par(mfrow = c(1, 1))    
+
+# Two ways to create the boxplot.
+boxplot(lean, obese, names = c("Lean","Obese"))
+boxplot(energy$expend ~ energy$stature, names = c("Lean","Obese"))
+
+# Create four stripcharts.
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(2,2), mex = 0.8, mar = c(3, 3, 2, 1) + .1)
+
+stripchart(energy$expend ~ energy$stature)
+stripchart(energy$expend ~ energy$stature, method = "stack")
+stripchart(energy$expend ~ energy$stature, method = "jitter")
+stripchart(energy$expend ~ energy$stature, method = "jitter", jitter = .03)
+
+par(opar)
+
+
 
 
