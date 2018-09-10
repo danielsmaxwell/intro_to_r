4871464101b08e2d0d0cad04ce751e02382ddd5d
ufarcs <36966787+ufarcs@users.noreply.github.com>
2018-09-10 11:25:14 -0400

    Updated script to handle Python code chunks.

--- hideOutput.js
+++ hideOutput.js
@@ -6,18 +6,24 @@ $(document).ready(function() {
 
     // add button to source code chunks
     if ( $(this).hasClass('s') ) {
-      $('pre.r', this).prepend("<div class=\"showopt\">Show Source</div><br style=\"line-height:22px;\"/>");
+      $('pre.r', this).prepend("<div class=\"showopt\">Code</div><br style=\"line-height:22px;\"/>");
       $('pre.r', this).children('code').attr('class', 'folded');
+    }
+    
+    // add button to source code chunks for python
+    if ( $(this).hasClass('s') ) {
+      $('pre.python', this).prepend("<div class=\"showopt\">Code</div><br style=\"line-height:22px;\"/>");
+      $('pre.python', this).children('code').attr('class', 'folded');
     }
 
     // add button to output chunks
     if ( $(this).hasClass('o') ) {
-      $('pre:not(.r)', this).has('code').prepend("<div class=\"showopt\">Show Output</div><br style=\"line-height:22px;\"/>");
+      $('pre:not(.r)', this).has('code').prepend("<div class=\"showopt\">Output</div><br style=\"line-height:22px;\"/>");
       $('pre:not(.r)', this).children('code:not(r)').addClass('folded');
 
       // add button to plots
       $(this).find('img').wrap('<pre class=\"plot\"></pre>');
-      $('pre.plot', this).prepend("<div class=\"showopt\">Show Plot</div><br style=\"line-height:22px;\"/>");
+      $('pre.plot', this).prepend("<div class=\"showopt\">Plot</div><br style=\"line-height:22px;\"/>");
       $('pre.plot', this).children('img').addClass('folded');
 
     }
@@ -29,11 +35,16 @@ $(document).ready(function() {
   // function to toggle the visibility
   $('.showopt').click(function() {
     var label = $(this).html();
-    if (label.indexOf("Show") >= 0) {
-      $(this).html(label.replace("Show", "Hide"));
+    if (label.indexOf("Code") === 0) {
+      $(this).html(label.replace("Code", " Hide"));
+    } else if (label.indexOf("Hide") === 1) {
+      $(this).html(label.replace(" Hide", "Code"));
+    } else if (label.indexOf("Output") === 0) {
+      $(this).html(label.replace("Output", "Hide"));
     } else {
-      $(this).html(label.replace("Hide", "Show"));
+      $(this).html(label.replace("Hide", "Output"));
     }
+    
     $(this).siblings('code, img').slideToggle('fast', 'swing');
   });
 });
